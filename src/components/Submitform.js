20f7b5d482957c700d5e1f4fd4af3514d3c835b0
vishnu kolli <nvish6965@gmail.com>
2023-02-02 20:06:46 +0530

    same as before

--- src/components/Submitform.js
+++ src/components/Submitform.js
@@ -1,16 +1,22 @@
 import "./Submitform.css"
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
 
 function Submitform(){
     return(
         <div className='form'>
      <form>
-        <input className='input' placeholder='Add Task' />
+        <input className='input'  placeholder='Add Task' />
+        
         <button className='add-button' type='submit'>Add</button>
     </form>   
     
 </div>
+
     )
     
 }
+
+
 
 export default Submitform
